476b68c4d4788df9a0a2601646e4ba58b960290e
GailDev10 <GailDev10@gmail.com>
2022-06-15 21:03:55 -0400

    connect to mongodb

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,61 @@
+require('../models/database');
+const Category = require('../models/Category')
+
+
 //get homepage
 
 exports.homepage = async(req, res) => {
-    res.render('index', { title: 'Cooking Blog - Home'});
+
+    try {
+        const limitNumber = 5;
+        const categories = await Category.find( {} ).limit(limitNumber);
+
+
+        res.render('index', { title: 'Cooking Blog - Home', categories });
+
+    } catch (error) {
+        res.status(500).send({message: error.message || 'Error Occured'}) ;   
+    }
 }
+
+
+
+
+// async function insertDummyCategoryData(){
+//     try {
+//         await Category.insertMany( [
+//             {
+//                 'name': 'Thai',
+//                 'image': 'thai-food.jpg'
+//             },
+//             {
+//                 'name': 'American',
+//                 'image': 'american-food.jpg'
+//             },
+//             {
+//                 'name': 'Chinese',
+//                 'image': 'chinese-food.jpg'
+//             },
+//             {
+//                 'name': 'Mexican',
+//                 'image': 'mexican-food.jpg'
+//             },
+//             {
+//                 'name': 'Indian',
+//                 'image': 'indian-food.jpg'
+//             },
+//             {
+//                 'name': 'Spanish',
+//                 'image': 'spanish-food.jpg'
+//             },
+//             {
+//                 'name': 'Thai',
+//                 'image': 'thai-food.jpg'
+//             },
+//         ]);
+//     } catch (error) {
+//         console.log('err', + error)
+//     }
+// }
+
+// insertDummyCategoryData()

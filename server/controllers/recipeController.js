1160151dc178d86dcd1f5e325743623f2e3ecbeb
GailDev10 <GailDev10@gmail.com>
2022-06-20 19:56:11 -0400

    added individual categories

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -42,6 +42,25 @@ exports.exploreCategories = async(req, res) => {
     }
 }
 
+//get categories/id
+
+
+exports.exploreCategoriesById = async(req, res) => {
+
+    try {
+        let categoryId = req.params.id;
+        const limitNumber = 20;
+        const categoryById = await Recipe.find( { 'category': categoryId } ).limit(limitNumber);
+
+
+        res.render('categories', { title: 'Cooking Blog - Categories', categoryById });
+
+    } catch (error) {
+        res.status(500).send({message: error.message || 'Error Occured'}) ;   
+    }
+}
+
+
 //get recipe
 exports.exploreRecipe = async(req, res) => {
 
@@ -56,7 +75,6 @@ exports.exploreRecipe = async(req, res) => {
         res.status(500).send({message: error.message || 'Error Occured'}) ;   
     }
 }
-
 
 
 
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,7 +5,8 @@ const recipeController = require('../controllers/recipeController');
 //App Routes
 
 router.get('/', recipeController.homepage);
-router.get('/categories', recipeController.exploreCategories);
 router.get('/recipe/:id', recipeController.exploreRecipe);
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
 
 module.exports = router;
